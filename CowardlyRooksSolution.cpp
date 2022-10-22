74b4e17872d866e340ccfaad89362d871fe8008a
pawanpk87 <arowpk@gmail.com>
2022-10-22 13:12:27 +0530

    first commit

---
+++ CowardlyRooksSolution.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+  
+  int t; 
+  cin>>t;
+  while(t--)
+  {
+  	int n,m;
+  	cin>>n>>m;
+  	int tempM=m;
+  	while(m--)
+  	{
+  		int x,y;
+  		cin>>x>>y;
+  	}
+  	if(tempM < n)
+  		cout<<"YES"<<endl;
+  	else
+  		cout<<"NO"<<endl;
+  }
+  return 0;
+}
